5dbd34860d7a4f743faf382010f9cf01a2068c3d
frontendhan <rinat.iz.miassa@Gmail.com>
2023-11-27 21:53:10 +0500

    Исправил ошибку, из-за которой вебпак ругался при сборке.
    Добавил нужную строчку выбора мода в файл конфига вебпак.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
