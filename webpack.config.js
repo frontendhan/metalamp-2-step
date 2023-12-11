5d7a9723a79dc7184500476b3c68227ff7107d3b
frontendhan <rinat.iz.miassa@Gmail.com>
2023-12-11 22:33:11 +0500

    Настроил вебпак на многостраничную сборку..

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
     mode: 'development',
     entry: {
-        main: path.resolve(__dirname, './src/main.js'),
+        'ui-kit-colors-type': './src/pages/ui-kit-colors-type/index.js',
+        'ui-kit-form-elements': './src/pages/ui-kit-form-elements/index.js',
+        'ui-kit-cards': './src/pages/ui-kit-cards/index.js',
+        'ui-kit-headers-footers': './src/pages/ui-kit-headers-footers/index.js',
+        'landing-page': './src/pages/landing-page/index.js',
+        'search-room': './src/pages/search-room/index.js',
+        'room-details': './src/pages/room-details/index.js',
+        'registration': './src/pages/registration/index.js',
+        'sign-in': './src/pages/sign-in/index.js',
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'main.bundle.js',
+        filename: '[name].bundle.js',
     },
     module: {
         rules: [
@@ -31,8 +40,49 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.pug'),
-            filename: 'index.html',
+            template: './src/pages/ui-kit-colors-type/index.pug',
+            filename: 'ui-kit-colors-type/index.html',
+            chunks: ['ui-kit-colors-type']
         }),
-    ],
-};
+        new HtmlWebpackPlugin({
+            template: './src/pages/ui-kit-form-elements/index.pug',
+            filename: 'ui-kit-form-elements/index.html',
+            chunks: ['ui-kit-form-elements']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/ui-kit-cards/index.pug',
+            filename: 'ui-kit-cards/index.html',
+            chunks: ['ui-kit-cards']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/ui-kit-headers-footers/index.pug',
+            filename: 'ui-kit-headers-footers/index.html',
+            chunks: ['ui-kit-headers-footers']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/landing-page/index.pug',
+            filename: 'landing-page/index.html',
+            chunks: ['landing-page']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/search-room/index.pug',
+            filename: 'search-room/index.html',
+            chunks: ['search-room']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/room-details/index.pug',
+            filename: 'room-details/index.html',
+            chunks: ['room-details']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/registration/index.pug',
+            filename: 'registration/index.html',
+            chunks: ['registration']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/sign-in/index.pug',
+            filename: 'sign-in/index.html',
+            chunks: ['sign-in']
+        }),
+    ]
+}
